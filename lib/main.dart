5d78a7c54bc9356e53fcff1a35fdbca5d2a1c379
Yuki Nishimura <yukinkopen@gmail.com>
2023-01-28 00:58:04 +0900

    タブナビゲーションにて画面選択を可能にした

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:omurice/component/diaryFormatItem.dart';
+import 'package:omurice/diaryFormatChoiceScreen.dart';
 import 'component/bottomIconBar.dart';
+import 'sampleScreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +18,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(
+        title: 'Flutter Demo Home Page',
+      ),
     );
   }
 }
@@ -30,14 +35,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int selectedIndex = 0;
+  List<Widget> display = [
+    const Sample(label: "「日記を見る」ページ"),
+    const DiaryFormatChoiceScreen(),
+    const Sample(label: "サービス・支援ページ"),
+    const Sample(label: "ユーザーページ")
+  ];
+  bool isAppBarShowed = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      bottomNavigationBar: BottomIconBar(), //UIの確認のため、いづれ消す
-      body: Center(),
+      appBar: isAppBarShowed
+          ? AppBar(
+              title: Text(widget.title),
+            )
+          : null,
+      bottomNavigationBar: BottomIconBar(
+        selectedIndex: selectedIndex,
+        tabDataList: [
+          TabData(
+            icon: const Icon(Icons.mode_edit),
+            onPressed: () {
+              selectedIndex = 0;
+              isAppBarShowed = true;
+              setState(() {});
+            },
+            isFocused: selectedIndex == 0,
+          ),
+          TabData(
+            icon: const Icon(Icons.book),
+            onPressed: () {
+              selectedIndex = 1;
+              isAppBarShowed = false;
+              setState(() {});
+            },
+            isFocused: selectedIndex == 1,
+          ),
+          TabData(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              selectedIndex = 2;
+              isAppBarShowed = true;
+              setState(() {});
+            },
+            isFocused: selectedIndex == 2,
+          ),
+          TabData(
+            icon: const Icon(Icons.home),
+            onPressed: () {
+              selectedIndex = 3;
+              isAppBarShowed = true;
+              setState(() {});
+            },
+            isFocused: selectedIndex == 3,
+          ),
+        ],
+      ), //UIの確認のため、いづれ消す
+      body: display[selectedIndex],
     );
   }
 }
