c8e87995d6a1aac8f227f6b32aed697226cc4958
IkemotoNanako <chibencaizi@gmail.com>
2023-01-27 16:22:08 +0900

    トップ画面追加

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,56 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       bottomNavigationBar: BottomIconBar(), //UIの確認のため、いづれ消す
-      body: Center(),
+      body: Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('images/top-background.jpg'),
+            fit: BoxFit.cover,
+          )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Center(
+                  child: Text('むねをはって',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                      ))),
+              Center(
+                  child: Text('いきよう',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                      ))),
+              ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black12),
+                  onPressed: () {},
+                  child: Text("日記を書く",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ))),
+              ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black12),
+                  onPressed: () {},
+                  child: Text("ほかの人の日記を見る",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ))),
+              ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.black12),
+                  onPressed: () {},
+                  child: Text("支援制度・サービスを探す",
+                      style: TextStyle(
+                        color: Colors.white,
+                      )))
+            ],
+          )),
     );
   }
 }
