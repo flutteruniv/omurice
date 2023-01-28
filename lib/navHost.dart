d0f5e3d44b18139dbdfacaa688d2fc25e7633db5
Yuki Nishimura <yukinkopen@gmail.com>
2023-01-28 20:07:47 +0900

    トップページから各画面への遷移を追加

---
+++ lib/navHost.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:omurice/diaryFormatChoiceScreen.dart';
+import 'component/bottomIconBar.dart';
+import 'sampleScreen.dart';
+
+class NavHost extends StatefulWidget {
+  const NavHost({
+    super.key,
+    required this.title,
+    required this.selectedIndex,
+  });
+
+  final String title;
+  final int selectedIndex;
+
+  @override
+  State<NavHost> createState() => _NavHostState();
+}
+
+class _NavHostState extends State<NavHost> {
+  List<Widget> display = [
+    const Sample(label: "「日記を見る」ページ"),
+    const DiaryFormatChoiceScreen(),
+    const Sample(label: "サービス・支援ページ"),
+    const Sample(label: "ユーザーページ")
+  ];
+  int selectedIndex = 0;
+
+  @override
+  void initState() {
+    selectedIndex = widget.selectedIndex;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: isAppBarShowed(selectedIndex)
+          ? AppBar(
+              title: Text(widget.title),
+            )
+          : null,
+      bottomNavigationBar: BottomIconBar(
+        selectedIndex: selectedIndex,
+        tabDataList: [
+          TabData(
+            icon: const Icon(Icons.mode_edit),
+            onPressed: () {
+              setState(() {
+                selectedIndex = 0;
+              });
+            },
+            isFocused: selectedIndex == 0,
+          ),
+          TabData(
+            icon: const Icon(Icons.book),
+            onPressed: () {
+              setState(() {
+                selectedIndex = 1;
+              });
+            },
+            isFocused: selectedIndex == 1,
+          ),
+          TabData(
+            icon: const Icon(Icons.search),
+            onPressed: () {
+              setState(() {
+                selectedIndex = 2;
+              });
+            },
+            isFocused: selectedIndex == 2,
+          ),
+          TabData(
+            icon: const Icon(Icons.home),
+            onPressed: () {
+              setState(() {
+                selectedIndex = 3;
+              });
+            },
+            isFocused: selectedIndex == 3,
+          ),
+        ],
+      ), //UIの確認のため、いづれ消す
+      body: display[selectedIndex],
+    );
+  }
+
+  bool isAppBarShowed(int selectedIndex) {
+    return selectedIndex != 1;
+  }
+}
--- lib/pages/top.dart
+++ lib/pages/top.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:omurice/navHost.dart';
 
 class Top extends StatelessWidget {
   const Top({
@@ -8,54 +9,97 @@ class Top extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: const BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage('images/top-background.jpg'),
-          fit: BoxFit.cover,
-        )),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            const SizedBox(
-              height: 80,
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage('images/top-background.jpg'),
+        fit: BoxFit.cover,
+      )),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          const SizedBox(
+            height: 80,
+          ),
+          const Center(
+            child: Text(
+              'むねをはって',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
             ),
-            const Center(
-                child: Text('むねをはって',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                    ))),
-            const Center(
-                child: Text('いきよう',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                    ))),
-            ElevatedButton(
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.black12),
-                onPressed: () {},
-                child: const Text("日記を書く",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ))),
-            ElevatedButton(
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.black12),
-                onPressed: () {},
-                child: const Text("ほかの人の日記を見る",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ))),
-            ElevatedButton(
-                style:
-                    ElevatedButton.styleFrom(backgroundColor: Colors.black12),
-                onPressed: () {},
-                child: const Text("支援制度・サービスを探す",
-                    style: TextStyle(
-                      color: Colors.white,
-                    )))
-          ],
-        ));
+          ),
+          const Center(
+            child: Text(
+              'いきよう',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.black12),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const NavHost(
+                    title: "",
+                    selectedIndex: 1,
+                  ),
+                ),
+              );
+            },
+            child: const Text(
+              "日記を書く",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.black12),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const NavHost(
+                    title: "",
+                    selectedIndex: 0,
+                  ),
+                ),
+              );
+            },
+            child: const Text(
+              "ほかの人の日記を見る",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.black12),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const NavHost(
+                    title: "",
+                    selectedIndex: 2,
+                  ),
+                ),
+              );
+            },
+            child: const Text(
+              "支援制度・サービスを探す",
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
