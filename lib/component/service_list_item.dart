f2bff1f867f06bea0d09e5ca576da25c0f7d6508
IkemotoNanako <chibencaizi@gmail.com>
2023-02-26 18:45:26 +0900

    サービス一覧画面

---
+++ lib/component/service_list_item.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ServiceListItem extends StatelessWidget {
+  const ServiceListItem({
+    Key? key,
+    // required this.id,
+    // required this.created_at,
+    required this.name,
+    // required this.url,
+    required this.thumbnail_url,
+    // required this.category_id,
+    // required this.max_age,
+    // required this.min_age,
+    // required this.start_date,
+    // required this.end_date,
+    // required this.prefecture_id,
+    // required this.restriction_id,
+    // required this.operator_type_id,
+    // required this.occupation_id,
+    // required this.income_limit,
+    // required this.can_entry,
+  }) : super(key: key);
+
+  // final int id;
+  // final created_at;
+  final String name;
+  // final String url;
+  final String thumbnail_url;
+  // final int category_id;
+  // final int max_age;
+  // final int min_age;
+  // final start_date;
+  // final end_date;
+  // final int prefecture_id;
+  // final int restriction_id;
+  // final int operator_type_id;
+  // final int occupation_id;
+  // final int income_limit;
+  // final bool can_entry;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: Column(children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(name),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                width: 100,
+                child: Image.network(thumbnail_url),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text("説明"),
+              )
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+}
--- lib/pages/service_list_screen.dart
+++ lib/pages/service_list_screen.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
 
+import '../component/search_widget.dart';
+import '../component/service_list.dart';
+
 class ServiceListScreen extends StatelessWidget {
   const ServiceListScreen({Key? key, required this.label}) : super(key: key);
   final String label;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        label,
+    final focusNode = FocusNode();
+    return DefaultTabController(
+      length: 4,
+      child: Focus(
+        focusNode: focusNode,
+        child: GestureDetector(
+          onTap: focusNode.requestFocus,
+          child: Scaffold(
+            appBar: AppBar(
+              elevation: 4,
+              backgroundColor: Colors.white,
+              automaticallyImplyLeading: false,
+              flexibleSpace: const Center(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 40),
+                  child: SearchWidget(
+                    hint: "キーワードで検索",
+                  ),
+                ),
+              ),
+              bottom: TabBar(
+                labelColor: Colors.black,
+                labelStyle: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                onTap: (value) => {
+                  focusNode.requestFocus(),
+                },
+                tabs: const [
+                  Tab(text: "お金"),
+                  Tab(text: "仕事"),
+                  Tab(text: "生活"),
+                  Tab(text: "相談"),
+                ],
+              ),
+            ),
+            body: const TabBarView(
+              children: [
+                ServiceList(mode: Mode.myself),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
