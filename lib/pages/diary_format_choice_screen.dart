c6446a33dd6d186a395df61be9f88c4aa067dcec
Kuroneko-mayuge <kfrmts0706@gmail.com>
2023-03-19 22:39:54 +0900

    add Google ADMob

--- lib/pages/diary_format_choice_screen.dart
+++ lib/pages/diary_format_choice_screen.dart
@@ -56,9 +56,9 @@ class _DiaryFormatChoiceScreenState extends State<DiaryFormatChoiceScreen> {
                 },
               ),
             ),
-            const Row(
+            Row(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [
+              children: const [
                 SizedBox(width: 50),
                 Text("特別な日記"),
               ],
